bd43d29ae3f151dbb569bda8d789e049dbcd4d70
spectators <39278009+spectators@users.noreply.github.com>
2018-05-17 12:04:48 -0700

    temp solution fix later + experiments

--- code/desktop/src/com/qxbytes/afterspace/desktop/DesktopLauncher.java
+++ code/desktop/src/com/qxbytes/afterspace/desktop/DesktopLauncher.java
@@ -7,7 +7,8 @@ import com.qxbytes.afterspace.SpaceGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.foregroundFPS = 30;
+		config.foregroundFPS = 120;
+		//	originally- config.foregroundFPS = 30; experimenting.
 		new LwjglApplication(new SpaceGame(), config);
 	}
 }
