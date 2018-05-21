58786f3418166afa6dfc12b7e876fceb5852d34a
spectators <edward.choi24@gmail.com>
2018-05-21 16:57:06 -0700

    New libgdx project.

---
+++ desktop/src/com/qxbytes/spacegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.qxbytes.spacegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.qxbytes.spacegame.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
