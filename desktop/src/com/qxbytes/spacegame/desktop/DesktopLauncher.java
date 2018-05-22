80af0036c7b70a128898d07dc754b68b429845e2
spectators <edward.choi24@gmail.com>
2018-05-21 17:29:08 -0700

    cap fps to 120 for now.

--- desktop/src/com/qxbytes/spacegame/desktop/DesktopLauncher.java
+++ desktop/src/com/qxbytes/spacegame/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.qxbytes.spacegame.SpaceGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.backgroundFPS = 30;	//	power saver
+		config.foregroundFPS = 120;	//	nobody needs more than 120fps in this game. 
+		config.vSyncEnabled = false; // here we go.	
 		new LwjglApplication(new SpaceGame(), config);
 	}
 }
