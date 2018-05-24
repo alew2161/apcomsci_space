fb96813a90d17ecf75c84184b2eb397b9310dd9f
spectators <edward.choi24@gmail.com>
2018-05-23 18:53:28 -0700

    a functional kinda main menu.
     コミット予定の変更点:
    	new file:   core/assets/data/default.fnt
    	new file:   core/assets/data/default.png
    	new file:   core/assets/data/uiskin.atlas
    	new file:   core/assets/data/uiskin.json
    	new file:   core/assets/data/uiskin.png
    	modified:   core/src/com/qxbytes/screens/MainMenuScreen.java
    	modified:   core/src/com/qxbytes/spacegame/SpaceGame.java

--- core/src/com/qxbytes/screens/MainMenuScreen.java
+++ core/src/com/qxbytes/screens/MainMenuScreen.java
@@ -5,31 +5,58 @@ import com.qxbytes.screens.GameScreen;
 import com.qxbytes.spacegame.SpaceGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 
 public class MainMenuScreen implements Screen {
 
-//	private SpriteBatch batch = game.getBatch();
+	private Skin skin;
+    private Stage stage;
+	SpaceGame game;
+	
+	public MainMenuScreen(SpaceGame game) 
+	{
+		this.game = game;
+	}
 	
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-/*        batch = new SpriteBatch();
-        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
         stage = new Stage();
 
-        final TextButton button = new TextButton("Click me", skin, "default");
+        final TextButton button = new TextButton("start", skin, "default");
         
         button.setWidth(200f);
         button.setHeight(20f);
-        button.setPosition(Gdx.graphics.getWidth() /2 - 100f, Gdx.graphics.getHeight()/2 - 10f);*/
+        button.setPosition(Gdx.graphics.getWidth() /2 - 100f, Gdx.graphics.getHeight()/2 - 10f);
+
+        button.addListener(new ClickListener(){
+            @Override 
+            public void clicked(InputEvent event, float x, float y){
+            	game.setScreen(new GameScreen(game));
+            }
+        });
+        
+        stage.addActor(button);
+        
+        Gdx.input.setInputProcessor(stage);
 	}
 
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        game.getBatch().begin();
+        stage.draw();
+        game.getBatch().end();
 		
 	}
 
@@ -60,7 +87,7 @@ public class MainMenuScreen implements Screen {
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		game.getBatch().dispose();
 	}
 
 }
