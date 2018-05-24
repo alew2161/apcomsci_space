f5429615e90256c976df0ff5278e1a1133d8f726
Metastable1883 <boson2003@gmail.com>
2018-05-24 13:20:29 -0700

    SpriteHandling is now working
    graphics class is now redundant for animations but  is still necessary for static entities

--- core/src/com/qxbytes/entities/SpriteHandling.java
+++ core/src/com/qxbytes/entities/SpriteHandling.java
@@ -1,26 +1,66 @@
 package com.qxbytes.entities;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
-public class SpriteHandling{
-	private static final int FRAME_COLS = 8, FRAME_ROWS = 8; //number of rows & columns on atlas
-	private Texture textureAtlas0;
-	public void create(String fileName) { 
-		textureAtlas0 = new Texture(Gdx.files.internal(fileName));
-		//need to use libgdx tool to configure atlas
-		TextureRegion[][] tmp = TextureRegion.split(textureAtlas0, 
-				((Graphics) textureAtlas0).getWidth() / FRAME_COLS,//finding height & width of sprite
-				((Graphics) textureAtlas0).getHeight() / FRAME_ROWS);
-		TextureRegion[] sprite0 = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-		int index = 0;
-		for (int i = 0; i < FRAME_ROWS; i++) {
-			for (int j = 0; j < FRAME_COLS; j++) {
-				sprite0[index++] = tmp[i][j];
+public class SpriteHandling {
+	private SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Sprite sprite;
+	private int currentFrame = 1;
+	private String currentAtlasKey = new String("0001");
+	/**
+	 * Constructor also renders animation so graphics class is redundant for animations
+	 * @param Atlas
+	 * @param SizeX
+	 * @param SizeY
+	 */
+	public SpriteHandling(String Atlas, int SizeX, int SizeY) {
+		create();
+		resize(SizeX, SizeY);
+		render();
+		dispose();
+	}
+
+	public void create() {        
+		batch = new SpriteBatch();
+		textureAtlas = new TextureAtlas(Gdx.files.internal("spritesheet.atlas"));
+		AtlasRegion region = textureAtlas.findRegion("0001");
+		sprite = new Sprite(region);
+		sprite.setPosition(120, 100);
+		sprite.scale(2.5f);
+		Timer.schedule(new Task(){
+			@Override
+			public void run() {
+				currentFrame++;
+				if(currentFrame > 20)
+					currentFrame = 1;
+				currentAtlasKey = String.format("%04d", currentFrame);
+				sprite.setRegion(textureAtlas.findRegion(currentAtlasKey));
 			}
 		}
-		new Animation<TextureRegion>(0.025f, sprite0);
+		,0,1/30.0f);
+	}
+
+	public void dispose() {
+		batch.dispose();
+		textureAtlas.dispose();
+	}
+
+	public void render() {        
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+	}
+
+	public void resize(int width, int height) {
 	}
 }
