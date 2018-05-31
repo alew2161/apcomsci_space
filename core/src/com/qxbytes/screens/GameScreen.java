bef667671a16d22439dfbd9e474b48ee3db97252
QxBytes <qxbytes@gmail.com>
2018-05-31 11:32:17 -0700

    Debug renderer ok

--- core/src/com/qxbytes/screens/GameScreen.java
+++ core/src/com/qxbytes/screens/GameScreen.java
@@ -200,7 +200,7 @@ public class GameScreen implements Screen {
 		 * Draw Everything now by passing the Batch in
 		 */
 		game.getBatch().end();
-		//debug.render(world, debugMatrix);
+		debug.render(world, debugMatrix);
 
 	}
 
